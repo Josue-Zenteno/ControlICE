7c523c04831b5ca525d1244f5ff013939c9b2c24
Josue-Zenteno <josuotp@gmail.com>
2021-04-02 15:56:11 +0200

    parser.py v3.0.0

--- src/Components/parser.py
+++ src/Components/parser.py
@@ -4,11 +4,18 @@
 
 class Parser:
     '''Parser Class'''
+    def parse_information(self, raw_measurements_dict):
+        '''Returns a Dictionary that contains only the
+           relevant information given for the sensors'''
+        return self.__dump_relevant_info(raw_measurements_dict)
+
     @staticmethod
-    def get_value_list(inDict):
-        final_dict = {}
-        for dct in inDict['values']['variable'][18:]:
-            if dct['id'][-1] == 't':
-                final_dict[dct['id']] = dct['value']
-        
-        return final_dict
+    def __dump_relevant_info(inDict):
+        '''Dumps into a new Dictionary only the total
+           measurement values (not partials) and ignores
+           sensor configuration values'''
+        outDict = {}
+        for internal_dict in inDict['values']['variable'][18:]:
+            if internal_dict['id'][-1] == 't':
+                outDict[internal_dict['id']] = internal_dict['value']
+        return outDict
