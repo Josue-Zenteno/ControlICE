eb0f2e45204d41db5f9fe0d35df6cd93becb7160
Josue-Zenteno <josuotp@gmail.com>
2021-04-02 15:52:52 +0200

    ControlICE.py v3.0.0

--- src/ControlICE.py
+++ src/ControlICE.py
@@ -1,22 +1,32 @@
-from Components.rest_manager import RESTManager
-from Components.xml_manager import XMLManager
-from Components.parser import Parser
-from Components.formatter import Formatter
-from Components.dispatcher import Dispatcher
+'''
+    ControlICE Application
+'''
+from art import tprint
+from Components.manager import Manager
 
-ITSI_PASILLO_URL = 'http://172.24.106.5/services/user/values.xml?id=ITSI+Pasillo'
-ITSI_CITISIM_URL = 'http://172.24.106.6/services/user/values.xml?id=ITSI+Citisim'
+class ControlICE:
+    def __init__(self):
+        '''Constructor'''
+        self.manager = Manager()
+        self.__print_app_welcome()
 
-rest_manager = RESTManager()
-xml_manager = XMLManager()
-parser = Parser()
-formatter = Formatter()
-dispatcher = Dispatcher()
+    def launch_controlICE(self):
+        '''Launches the application'''
+        self.manager.launch_app()
 
-while True:
-    xml = rest_manager.get_request(ITSI_PASILLO_URL)
-    dict = xml_manager.xlm2dict(xml)
-    parsed_dict = parser.get_value_list(dict)
-    _format = formatter.translate_to_influx(parsed_dict)
-    dispatcher.dispatch(_format)
+    @staticmethod
+    def __print_app_welcome():
+        '''Prints App welcome message'''
+        print("\n\n")
+        tprint("ControlICE",font = "larry3d")
+        print("CP.SL:\n- Josue Carlos Zenteno Yave"
+                    "\n- Sergio Silvestre Pavon"
+                    "\n- Alejandro Riquelme Castaño"
+                    "\n- Javier Santana Delgado"
+                    "\n- Julio Sanchez de las Heras Martin Consuegra\n")
 
+try:
+    controlICE = ControlICE()
+    controlICE.launch_controlICE()
+except KeyboardInterrupt:
+    pass
